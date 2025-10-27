779d4835dd0743923546a88097a37d0de0f7bff7
Brad Vincent <bradvin@gmail.com>
2025-10-27 21:01:09 +0000

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       FooGallery Demo Plugin
+ * Description:       Sets everything up to get FooGallery working in Playground.
+ * Version:           0.0.1
+ * Requires at least: 6.5
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// Make sure we do not need to optin.
+define( 'WP_FS__DEV_MODE', true );
+define( 'WP_FS__SKIP_EMAIL_ACTIVATION', true );
+
+add_action( 'init', function() { 
+    foogallery_create_demo_content(); 
+} );
