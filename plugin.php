252d896c609ddb3d2290c83f38a9524f4e25daff
Brad Vincent <bradvin@gmail.com>
2025-10-29 15:54:56 +0000

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -10,6 +10,49 @@ defined( 'ABSPATH' ) || exit;
 
 // Make sure we do not need to optin.
 define( 'FOOPLUGINS_FREEMIUS_ANONYMOUS', true );
+define( 'WP_FS__DEV_MODE', true );
+
+if ( !is_admin() ) return;
+
+// Add a basic settings page
+add_action( 'admin_menu', function() {
+	add_options_page(
+		'FooGallery Demo Settings',
+		'FooGallery Demo',
+		'manage_options',
+		'foogallery-demo-settings',
+		'foogallery_demo_render_settings_page'
+	);
+} );
+
+function foogallery_demo_render_settings_page() {
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	$constants = [
+		'FOOPLUGINS_FREEMIUS_ANONYMOUS',
+		'WP_DEBUG',
+		'WP_FS__DEV_MODE',
+		'WP_PLAYGROUND',
+	];
+
+	echo '<div class="wrap">';
+	echo '<h1>FooGallery Demo Settings</h1>';
+	echo '<table class="widefat striped">';
+	echo '<thead><tr><th>Constant</th><th>Value</th></tr></thead><tbody>';
+
+	foreach ( $constants as $constant ) {
+		$value = defined( $constant ) ? constant( $constant ) : '<em>Not defined</em>';
+		if ( is_bool( $value ) ) {
+			$value = $value ? 'true' : 'false';
+		}
+		echo '<tr><td>' . esc_html( $constant ) . '</td><td><code>' . esc_html( (string) $value ) . '</code></td></tr>';
+	}
+
+	echo '</tbody></table>';
+	echo '</div>';
+}
 
 // add_action( 'init', function() { 
 //     foogallery_create_demo_content(); 
